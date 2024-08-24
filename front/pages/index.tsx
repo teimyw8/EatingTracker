308498a5db7281f5dcdb491e135a327f56eeb0cb
Thomas Hynes <tjhynes8@icloud.com>
2024-08-24 17:23:30 -0500

    routing

---
+++ front/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+import About from '../pagesImpl/homeImpl'
+
+export default function Main() {
+    
+    return (
+      <About></About>
+    )
+  }
